3e0efbfbcd5057c96b050a4439a2ac740423d369
Jmz0127 <jmezz1@icloud.com>
2021-11-12 11:17:11 -0500

    added quick styling for legibility, additionally added text prompt when clicking order pizza or completion links in nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
+import './App.css'
 
 const App = () => {
   return (
@@ -8,10 +9,20 @@ const App = () => {
         <h1>Lambda Eats</h1>
         <nav>
           <Link to='/'>Home</Link>
-          <Link id='order-pizza'to='/pizza'>Order Form</Link>
+          <Link id='order-pizza'to='/pizza'>Order Pizza</Link>
           <Link to='/completion'>Completion</Link>
         </nav>
       </header>
+      <main>
+        <Switch>
+          <Route path='/completion'>
+            <div>completion</div>
+          </Route>
+          <Route path='/pizza'>
+            <div>order</div>
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 };

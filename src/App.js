50b26855890ea2a98bb9ba5dedc7ea3d13f48cf4
Jmz0127 <jmezz1@icloud.com>
2021-11-12 11:44:19 -0500

    set up initial key value pairs for our order form - name, size, topings x4, special

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { Link, Switch, Route } from 'react-router-dom';
-import './App.css'
+import './App.css' //only did this so the links are more legible and look a little better - personal stretch
+import Completion from './components/Completion.js'; //personal stretch - want to see a completion message post-ordering
+
+
+const initialOrderForm={
+  name:'', //SHOULD BE TEXT FIELD
+  size:'', //SHOULD BE DROPDOWN
+  topping1:false, //SHOULD BE CHECKBOX
+  topping2:false, //SHOULD BE CHECKBOX
+  topping3:false, //SHOULD BE CHECKBOX
+  topping4:false, //SHOULD BE CHECKBOX
+  special: '', //SHOULD BE TEXT FIELD
+}
+
 
 const App = () => {
   return (
@@ -15,8 +28,11 @@ const App = () => {
       </header>
       <main>
         <Switch>
+          <Route exact path='/'>
+            <div>home</div>
+          </Route>
           <Route path='/completion'>
-            <div>completion</div>
+            <Completion />
           </Route>
           <Route path='/pizza'>
             <div>order</div>
---
+++ src/components/Completion.js
@@ -0,0 +1,12 @@
+//personal stretch here - I want to see a order is complete type of thing once a user submits a pizza order
+import React from 'react';
+
+const Completion = (props) => {
+    console.log(props)
+
+    return (
+        <>thanks for your order! pizza will be in the oven soon. </>
+    )
+}
+
+export default Completion

11c6f2f8ae5f0da1b8c05c4e8ea5f05fcd1266e5
Jmz0127 <jmezz1@icloud.com>
2021-11-12 12:32:38 -0500

    fixed yup error by moving formschema validations to a seperate folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link, Switch, Route } from 'react-router-dom';
 import './App.css' //only did this so the links are more legible and look a little better - personal stretch
-import Completion from './components/Completion.js'; //personal stretch - want to see a completion message post-ordering
+import Completion from './components/Completion'; //personal stretch - want to see a completion message post-ordering
+import formSchema from './validation/formSchema';
 
-
-const initialOrderForm={
+//initial form values
+const initialOrderForm={ 
   name:'', //SHOULD BE TEXT FIELD
   size:'', //SHOULD BE DROPDOWN
   topping1:false, //SHOULD BE CHECKBOX
   topping2:false, //SHOULD BE CHECKBOX
   topping3:false, //SHOULD BE CHECKBOX
   topping4:false, //SHOULD BE CHECKBOX
-  special: '', //SHOULD BE TEXT FIELD
+  special: '' //SHOULD BE TEXT FIELD
+}
+
+//how to deal with errors
+const initialOrderFormErrors={ 
+  name:'', //SHOULD BE TEXT FIELD
+  size:'', //SHOULD BE DROPDOWN
+  topping1:'', //SHOULD BE CHECKBOX
+  topping2:'', //SHOULD BE CHECKBOX
+  topping3:'', //SHOULD BE CHECKBOX
+  topping4:'', //SHOULD BE CHECKBOX
+  special: '' //SHOULD BE TEXT FIELD
 }
 
 
